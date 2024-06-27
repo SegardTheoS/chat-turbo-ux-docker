c693f3c656e2ee330319a7273dffcf084a83bf5a
SegardTheo <segard.webstime@gmail.com>
2024-06-27 15:58:08 +0200

    add twig + doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
